a37ccdfc9bdbfa78715a4265a89d5ffa256f8de9
Mykyta Pavlov <pavlov2021mf12@student.karazin.ua>
2023-10-17 06:40:46 +0300

    initial setup

--- src/main/java/web/bookshop/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package web.bookshop.exception;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
