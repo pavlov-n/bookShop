14f18951cff921cceafd62d54963b82d621bf3bb
Mykyta Pavlov <pavlov2021mf12@student.karazin.ua>
2023-10-17 06:49:41 +0300

    init setup

--- src/main/java/web/bookshop/BookshopApplication.java
+++ src/main/java/web/bookshop/BookshopApplication.java
@@ -1,12 +1,7 @@
 package web.bookshop;
 
-import web.bookshop.model.Book;
-import web.bookshop.service.BookService;
-import java.math.BigDecimal;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookshopApplication {
